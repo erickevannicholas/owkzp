2765d5683ffb8c98dc7ca60435ffc10816429e7e
erickevannicholas <84986826+erickevannicholas@users.noreply.github.com>
2022-06-18 18:20:28 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,5 @@
-cd CREDS
-docker build . -t erikfebi
-docker run erikfebi
+git clone https://github.com/TeamYukki/YukkiMusicBot /root/musicjato
+cp CREDS/.env /root/musicjato/.env
+cd /root/musicjato
+docker build . -t jatomusic
+docker run jatomusic
